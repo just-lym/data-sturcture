fe65fd93e6bd5ddbd8f7a96caa11f5550a5cf4d3
liyaming <li253870699@gmail.com>
2023-08-30 22:23:55 +0800

    堆排序

--- src/main/java/com/justlym/dataStructure/sort/RangeHeapSort.java
+++ src/main/java/com/justlym/dataStructure/sort/RangeHeapSort.java
@@ -1,8 +1,28 @@
 package com.justlym.dataStructure.sort;
 
+import java.util.PriorityQueue;
+
 /**
  * @author: justLym
  * 2023/08/30 17:30
  */
 public class RangeHeapSort {
+
+    public void rangeHeapSort(int[] arr, int k) {
+        if (arr == null || arr.length == 0) {
+            return;
+        }
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for (int i = 0; i < k; i++) {
+            queue.offer(arr[i]);
+        }
+        int i = 0;
+        for (int j = k; j < arr.length - 1; j++) {
+            queue.offer(arr[j]);
+            arr[i++] = queue.poll();
+        }
+        while (!queue.isEmpty()) {
+            arr[i++] = queue.poll();
+        }
+    }
 }
---
+++ src/test/java/com/justlym/dataStructure/sort/RangeHeapSortTest.java
@@ -0,0 +1,15 @@
+package com.justlym.dataStructure.sort;
+
+import org.junit.Test;
+
+/**
+ * @author: justLym
+ * 2023/08/30 22:18
+ */
+public class RangeHeapSortTest {
+
+    @Test
+    public void rangeHeapSortTest() {
+
+    }
+}
